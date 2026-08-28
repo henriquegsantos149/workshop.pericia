14d3cd058b6c8faf87586a6ea19595f88b7b2019
henriquegsantos149 <henriquegonzalez@poli.ufrj.br>
2026-08-28 09:28:23 -0300

    feat: configure clean /lp2 route via lp2/index.html

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        lp2: resolve(__dirname, 'lp2.html')
+        lp2: resolve(__dirname, 'lp2/index.html')
       }
     }
   }
